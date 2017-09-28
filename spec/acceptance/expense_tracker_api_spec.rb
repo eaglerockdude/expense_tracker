c85c4ea58df4de02d92a5c05a1253a278fa58d44
ken mcfadden <kendmcfadden@gmail.com>
2017-09-27 20:18:32 -0700

    added sinatra route & api.rb starter

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -10,13 +10,16 @@ module ExpenseTracker
     def app
       ExpenseTracker::API.new
     end
-    it 'records submitted expenses' do
+    it 'captures/records submitted expenses' do
       coffee = {
           'payee' => 'starbucks',
           'amount' => '5.75',
           'date' => '2017-09-18'
       }
       post 'expenses', JSON.generate(coffee)
+      expect(last_response.status).to eq(200)
+      parsed = JSON.parse(last_response.body)
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
     end
   end
 end
